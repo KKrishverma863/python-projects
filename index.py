5cafe2e4c23eb2ffd48c7f231de678da03b24345
KKrishverma863 <k61869483@gmail.com>
2025-02-08 19:40:25 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,8 @@
+# Create a program that asks for two number and perform addition , substraction , multiplication and division .
+a = int(input("Enter 1st number :")) 
+b = int(input("Enter 2nd number :"))
+print("sum :" , a + b  )
+print("difference:", a - b)
+print("multiplication:", a* b )
+print("division:", a/b)
 
