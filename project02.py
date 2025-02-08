4fa43c2f34ecea009f0817c5d34e04ca5fe5c7ac
KKrishverma863 <k61869483@gmail.com>
2025-02-08 19:27:09 +0530

    Create project02.py

---
+++ project02.py
@@ -0,0 +1,6 @@
+# Write a program to convert temperature from celsius to fahrenheit. formula = (C x 9 / 5)+ 32
+
+
+temp_celsius = float(input("Enter temperature in celsius :"))
+temp_fahrenheit = ( temp_celsius * 9 / 5 ) + 32 
+print("temp_fahrenheit :", temp_fahrenheit)
