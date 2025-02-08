b7b67ece94fc00d91a0288cb58aec3dba726d770
KKrishverma863 <k61869483@gmail.com>
2025-02-08 19:36:50 +0530

    Create project03.py

---
+++ project03.py
@@ -0,0 +1,6 @@
+# Write a program to calculate the area of a circle . prompt the user to  input the radius . formula : Area = math.pi (r ** 2)
+
+import math
+radius = float(input("Enter the radius :"))
+area =(math.pi)*(radius ** 2)
+print("Area :", area)
